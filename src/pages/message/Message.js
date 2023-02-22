26f827960b8573d7127a1418f2ad3a17464a7374
AK-Polash <polashk199@gmail.com>
2023-02-23 01:13:30 +0600

    dynamically added the Alert without sending Message

--- src/pages/message/Message.js
+++ src/pages/message/Message.js
@@ -9,6 +9,7 @@ import {
   Button,
   IconButton,
   Container,
+  Alert,
 } from "@mui/material";
 import PhotoCamera from "@mui/icons-material/PhotoCamera";
 import "./message.css";
@@ -38,124 +39,145 @@ const Message = () => {
           <Friends />
         </Grid>
 
-        <Grid item xs={7.6} sx={{ display: "" }}>
-          <Box
-            sx={{
-              borderRadius: "8px",
-              overflow: "hidden",
-              boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
-              padding: "0 30px",
-            }}
-          >
-            <ProfileContentHeading
-              userAs="active"
-              title={
-                data.activeChat.focusedItem
-                  ? data.userData.userInfo.uid ===
-                    data.activeChat.focusedItem.senderId
-                    ? data.activeChat.focusedItem.receiverName
-                    : data.activeChat.focusedItem.senderName
-                  : "Default Khan"
-              }
-              photoURL={
-                data.activeChat.focusedItem
-                  ? data.userData.userInfo.uid ===
-                    data.activeChat.focusedItem.senderId
-                    ? data.activeChat.focusedItem.receiverPhoto
-                    : data.activeChat.focusedItem.senderPhoto
-                  : ""
-              }
-            />
-            <Divider />
-
+        <Grid
+          item
+          xs={7.6}
+          sx={{
+            display: data.activeChat.focusedItem ? "block" : "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {data.activeChat.focusedItem ? (
             <Box
               sx={{
-                width: "100%",
-                height: "480px",
-                background: "#fff",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                rowGap: "10px",
-                padding: "15px 30px",
-                overflowY: "scroll",
-                overflowX: "hidden",
-                position: "relative",
+                borderRadius: "8px",
+                overflow: "hidden",
+                boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
+                padding: "0 30px",
               }}
             >
-              <div className="receiver">
-                <div className="chat receiver__chat"> Receiver </div>
-                <div className="chat__moment"> Just now </div>
-              </div>
+              <ProfileContentHeading
+                userAs="active"
+                title={
+                  data.activeChat.focusedItem
+                    ? data.userData.userInfo.uid ===
+                      data.activeChat.focusedItem.senderId
+                      ? data.activeChat.focusedItem.receiverName
+                      : data.activeChat.focusedItem.senderName
+                    : "Default Khan"
+                }
+                photoURL={
+                  data.activeChat.focusedItem
+                    ? data.userData.userInfo.uid ===
+                      data.activeChat.focusedItem.senderId
+                      ? data.activeChat.focusedItem.receiverPhoto
+                      : data.activeChat.focusedItem.senderPhoto
+                    : ""
+                }
+              />
+              <Divider />
 
-              <div className="sender">
-                <div className="chat sender__chat"> Sender </div>
-                <div className="chat__moment"> Just now </div>
-              </div>
-            </Box>
-
-            <Divider />
-            <div className="input__main">
-              <Container
+              <Box
                 sx={{
+                  width: "100%",
+                  height: "480px",
+                  background: "#fff",
                   display: "flex",
-                  columnGap: "20px",
-                  justifyContent: "center",
+                  flexDirection: "column",
                   alignItems: "center",
+                  justifyContent: "center",
+                  rowGap: "10px",
+                  padding: "15px 30px",
+                  overflowY: "scroll",
+                  overflowX: "hidden",
                   position: "relative",
                 }}
               >
-                <TextField
-                  placeholder="type message"
-                  size="small"
-                  multiline={false}
-                  name="sms"
-                  onChange={(e) =>
-                    setFormData({ ...formData, sms: e.target.value })
-                  }
-                  value={formData.sms}
-                  sx={{
-                    width: "80%",
-                    background: "#F1F1F1",
-                  }}
-                />
+                <div className="receiver">
+                  <div className="chat receiver__chat"> Receiver </div>
+                  <div className="chat__moment"> Just now </div>
+                </div>
 
-                <IconButton
-                  color="primary"
-                  aria-label="upload picture"
-                  component="label"
+                <div className="sender">
+                  <div className="chat sender__chat"> Sender </div>
+                  <div className="chat__moment"> Just now </div>
+                </div>
+              </Box>
+
+              <Divider />
+              <div className="input__main">
+                <Container
                   sx={{
-                    position: "absolute",
-                    top: "50%",
-                    right: "190px",
-                    transform: "translateY(-50%)",
+                    display: "flex",
+                    columnGap: "20px",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    position: "relative",
                   }}
                 >
-                  <input
-                    hidden
-                    accept="image/*"
-                    type="file"
-                    name="photo"
+                  <TextField
+                    placeholder="type message"
+                    size="small"
+                    multiline={false}
+                    name="sms"
                     onChange={(e) =>
-                      setFormData({ ...formData, photo: e.target.value })
+                      setFormData({ ...formData, sms: e.target.value })
                     }
+                    value={formData.sms}
+                    sx={{
+                      width: "80%",
+                      background: "#F1F1F1",
+                    }}
                   />
-                  <PhotoCamera />
-                </IconButton>
 
-                <BsEmojiSmile className="emoji__icon" />
+                  <IconButton
+                    color="primary"
+                    aria-label="upload picture"
+                    component="label"
+                    sx={{
+                      position: "absolute",
+                      top: "50%",
+                      right: "190px",
+                      transform: "translateY(-50%)",
+                    }}
+                  >
+                    <input
+                      hidden
+                      accept="image/*"
+                      type="file"
+                      name="photo"
+                      onChange={(e) =>
+                        setFormData({ ...formData, photo: e.target.value })
+                      }
+                    />
+                    <PhotoCamera />
+                  </IconButton>
 
-                <Button
-                  type="submit"
-                  variant="contained"
-                  onClick={handleSubmit}
-                >
-                  <TbSend className="send__icon" />
-                </Button>
-              </Container>
-            </div>
-          </Box>
+                  <BsEmojiSmile className="emoji__icon" />
+
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    onClick={handleSubmit}
+                  >
+                    <TbSend className="send__icon" />
+                  </Button>
+                </Container>
+              </div>
+            </Box>
+          ) : (
+            <Alert
+              severity="info"
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              Select Friends to Send Message..!
+            </Alert>
+          )}
         </Grid>
       </Grid>
     </>
