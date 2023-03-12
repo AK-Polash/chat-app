6ce7d9583d45de51dfab8301b8163185e51d0b76
AK-Polash <polashk199@gmail.com>
2023-03-13 05:36:41 +0600

    New Firebase Config added here...
    Changed the Old Firebase Config file as there was a problem in Firebase

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,19 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBtSRRsQJtisifzXTbx-TNmUyDb4UXrwL8",
-  authDomain: "chat-app-7c7e4.firebaseapp.com",
-  projectId: "chat-app-7c7e4",
-  storageBucket: "chat-app-7c7e4.appspot.com",
-  messagingSenderId: "88402179103",
-  appId: "1:88402179103:web:d6774ce5160ef7f1a7112a",
+  apiKey: "AIzaSyCd7Ws0KDEQy9bSzbJyMHzWIgoDdSgV-H8",
+  authDomain: "chatting-app-fd011.firebaseapp.com",
+  projectId: "chatting-app-fd011",
+  storageBucket: "chatting-app-fd011.appspot.com",
+  messagingSenderId: "225189874049",
+  appId: "1:225189874049:web:83ae8618896b61ab3bc080",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export default firebaseConfig;
